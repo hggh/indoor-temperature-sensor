7dd968020ef613ef84cf22a3e26a19600147da3d
Jonas Genannt <jonas@brachium-system.net>
2020-06-05 10:20:51 +0200

    new wip code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include "driver/adc.h"
+#include <esp_wifi.h>
 
 #define uS_TO_S_FACTOR 1000000
 #define ENABLE_GxEPD2_GFX 0
@@ -10,20 +11,63 @@
 
 #include <PubSubClient.h>
 
-#include "DHTesp.h"
-DHTesp dht;
-int dhtPin = 22;
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 #include <Fonts/FreeSans18pt7b.h>
+#include <Fonts/FreeSerif12pt7b.h>
 #include <Fonts/FreeSansBold24pt7b.h>
 
 #include "config.h"
 
-GxEPD2_BW<GxEPD2_154, GxEPD2_154::HEIGHT> display(GxEPD2_154(SS, 17, 16, 4));
+/*
+ *  DIN  = GPIO 23
+ *  CLK  = GPIO 18
+ *  CS   = GPIO  5
+ *  DC   = GPIO 17
+ *  RST  = GPIO 16
+ *  BUSY = GPIO  4
+ *
+ *  BME280
+ *  SDA = GPIO 21
+ *  SCL = GPIO 22
+ *
+ *  Touch Sensor
+ *  T9  = GPIO 32 aka 33?!?(wrong label on breakout?)
+ *  
+ *  Voltage Input  = GPIO 35 / ADC7
+ *  ENABLE Voltage = GPIO 19 
+ */
+GxEPD2_BW<GxEPD2_154, GxEPD2_154::HEIGHT> display(GxEPD2_154(5, 17, 16, 4));
 RTC_DATA_ATTR unsigned int bootCount = 0;
 WiFiClient espClient;
 PubSubClient client(espClient);
 String hostname = String(HOSTNAME);
+const short pin_touch_pad = T9;
+const short pin_enable_voltage_divider = 19;
+const short pin_battery_adc = 35;
+const float volt_r1 = 10000.0;
+const float volt_r2 = 10000.0;
+const float voltage = 3.3;
+Adafruit_BME280 bme;
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
 
 void mqtt_check_connection() {
   if (!client.connected()) {
@@ -35,14 +79,34 @@ void wifi_got_ip_event(WiFiEvent_t event, WiFiEventInfo_t info) {
   mqtt_check_connection();
 }
 
+float get_battery_voltage() {
+  digitalWrite(pin_enable_voltage_divider, HIGH);
+  float val = 0.0;
+
+  val = analogRead(pin_battery_adc);
+  float vin = ((val * voltage) / 4095.0) / (volt_r2 / ( volt_r1 + volt_r2));
+
+  digitalWrite(pin_enable_voltage_divider, LOW);
+
+  return vin;
+}
+
+void touch_interrupt() {
+}
+
 void setup() {
 #ifdef DEBUG
   Serial.begin(115200);
   Serial.println("Booting...");
 #endif
 
-  adc_power_off();
   btStop();
+  adc_power_on();
+  esp_wifi_start();
+
+  pinMode(pin_battery_adc, INPUT);
+  pinMode(pin_enable_voltage_divider, OUTPUT);
+  digitalWrite(pin_enable_voltage_divider, LOW);
 
   WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
   WiFi.onEvent(wifi_got_ip_event, WiFiEvent_t::SYSTEM_EVENT_STA_GOT_IP);
@@ -52,7 +116,14 @@ void setup() {
 
   client.setServer(MQTT_SERVER, 1883);
 
-  dht.setup(dhtPin, DHTesp::DHT22);
+  unsigned status = bme.begin(0x76, &Wire);
+#ifdef DEBUG
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+    Serial.print("SensorID was: 0x");
+    Serial.println(bme.sensorID(),16);
+  }
+#endif
 
   display.init(0, false);
   display.setRotation(2);
@@ -72,6 +143,12 @@ void setup() {
   display.setCursor(130, 185);
   display.print("hPa");
 
+#ifdef DEBUG
+  display.setFont(&FreeSerif12pt7b);
+  display.setCursor(90, 80);
+  display.print("bootCount");
+#endif
+
   if (bootCount == 0) {
     display.display();
   }
@@ -80,19 +157,49 @@ void setup() {
   }
   ++bootCount;
   display.hibernate();
+
+print_wakeup_reason();
+
+  touchAttachInterrupt(pin_touch_pad, touch_interrupt, 30);
 }
 
 void loop() {
-  TempAndHumidity newValues = dht.getTempAndHumidity();
-  
-  float h = newValues.humidity;
-  float t = newValues.temperature;
-  float p = random(900, 1111);
-  if (dht.getStatus() != 0) {
-    Serial.println("error dht");
-    delay(200);
-    return;
+  float h = bme.readHumidity();
+  float t = bme.readTemperature();
+  float p = bme.readPressure() / 100.0F;
+#ifdef DEBUG
+  Serial.print("bme.readHumidity: ");
+  Serial.println(h);
+  Serial.print("bme.readTemperature: ");
+  Serial.println(t);
+  Serial.print("bme.readPressure: ");
+  Serial.println(p);
+#endif
+  // TODO Timeout for wifi and mqtt
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(10);
+    Serial.print(".");
   }
+  mqtt_check_connection();
+
+  //float battery_voltage = get_battery_voltage();
+  float battery_voltage = 3.3;
+
+  client.publish(String("/temperature/" + hostname + "/state").c_str(), String(t, 2).c_str());
+  client.publish(String("/humidity/" + hostname + "/state").c_str(), String(h, 2).c_str());
+  client.publish(String("/pressure/" + hostname + "/state").c_str(), String(p, 2).c_str());
+  client.publish(String("/batteryvoltage/" + hostname + "/state").c_str(), String(battery_voltage).c_str());
+  client.publish(String("/bootcount/" + hostname + "/state").c_str(), String(bootCount).c_str());
+
+  delay(1);
+  espClient.flush();
+  delay(1);
+
+#ifdef DEBUG
+  display.setCursor(5, 80);
+  display.setFont(&FreeSerif12pt7b);
+  display.print(bootCount, 1);
+#endif
 
   display.setFont(&FreeSansBold24pt7b);
   display.setCursor(5, 50);
@@ -110,27 +217,17 @@ void loop() {
   display.display(true);
   display.hibernate();
 
-  // TODO Timeout for wifi and mqtt
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(10);
-    Serial.print(".");
-  }
-  mqtt_check_connection();
+Serial.print("Volt: ");
+Serial.println(battery_voltage);
 
-  client.publish(String("/temperature/" + hostname + "/state").c_str(), String(t, 2).c_str());
-  client.publish(String("/humidity/" + hostname + "/state").c_str(), String(h, 2).c_str());
-  client.publish(String("/pressure/" + hostname + "/state").c_str(), String(p, 2).c_str());
-  client.publish(String("/bootcount/" + hostname + "/state").c_str(), String(bootCount).c_str());
-
-  espClient.flush();
-  delay(1);
   client.disconnect();
   delay(1);
   espClient.flush();
   delay(1);
   WiFi.disconnect(true);
 
-  esp_sleep_enable_timer_wakeup(30 * uS_TO_S_FACTOR);
+  esp_wifi_stop();
+  esp_sleep_enable_timer_wakeup(9 * 60  * uS_TO_S_FACTOR);
+  esp_sleep_enable_touchpad_wakeup();
   esp_deep_sleep_start();
-
 }
